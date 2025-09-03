39df613ea22f4ae1a722670acccef2ac6c03dc9d
44ntunes <atosprinci@gmail.com>
2025-09-02 21:59:42 -0300

    Adicionamos verificações para garantir que ao menos um dos campos textBody ou htmlBody tenha conteúdo. Caso contrário, o erro ArgumentError é lançado.

--- bin/meu_servico_de_email.dart
+++ bin/meu_servico_de_email.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
- 
+
 // Função para enviar e-mail
 Future<void> enviarEmailViaBackend(String to, String subject, {String? textBody, String? htmlBody}) async {
   final smtpUser = Platform.environment['SMTP_USER'];
@@ -10,27 +10,27 @@ Future<void> enviarEmailViaBackend(String to, String subject, {String? textBody,
   if (smtpUser == null || smtpPass == null) {
     throw StateError('Variáveis SMTP_USER ou SMTP_PASS não definidas');
   }
- 
+
   final smtpServer = SmtpServer(
-'smtp.gmail.com',
+    'smtp.gmail.com',
     username: smtpUser,
     password: smtpPass,
     port: 587,
     ssl: false,
   );
- 
+
   final message = Message()
     ..from = Address(smtpUser, 'ImTrouble')
     ..recipients.add(to)
     ..subject = subject;
- 
+
   if (textBody != null && textBody.isNotEmpty) {
     message.text = textBody;
   }
   if (htmlBody != null && htmlBody.isNotEmpty) {
     message.html = htmlBody;
   }
- 
+
   try {
     final sendReport = await send(message, smtpServer);
     print('E-mail enviado: ${sendReport.toString()}');
@@ -38,32 +38,32 @@ Future<void> enviarEmailViaBackend(String to, String subject, {String? textBody,
     print('Erro ao enviar e-mail: $e');
   }
 }
- 
+
 // Função que recebe dados via HTTP e envia o e-mail
 Future<void> handleRequest(HttpRequest request) async {
   final data = await utf8.decoder.bind(request).join();
   final jsonBody = jsonDecode(data);
- 
+
   final to = jsonBody['to'] as String;
   final subject = jsonBody['subject'] as String;
   final textBody = jsonBody['text'] as String?;
   final htmlBody = jsonBody['html'] as String?;
- 
+
   if ((textBody == null || textBody.isEmpty) && (htmlBody == null || htmlBody.isEmpty)) {
     throw ArgumentError('Nenhum conteúdo para enviar: texto ou html deve ser informado.');
   }
- 
+
   await enviarEmailViaBackend(to, subject, textBody: textBody, htmlBody: htmlBody);
 }
- 
+
 // Função principal que configura o servidor HTTP
 void main() async {
   final portEnv = Platform.environment['PORT'];
   final port = portEnv != null ? int.parse(portEnv) : 8080;
- 
+
   final server = await HttpServer.bind(InternetAddress.anyIPv4, port);
   print('Servidor rodando na porta $port');
- 
+
   await for (final request in server) {
     if (request.method == 'POST') {
       try {
